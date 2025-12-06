c405fc535465336ee6df06b31bebe46f25b8780d
jdiegovalerozzz <juandvalerox1@gmail.com>
2025-12-05 23:25:49 -0400

    Fix: cors issues

--- back/src/server.js
+++ back/src/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
 const petsRouter = require('./routes/pets');
 const sellsRouter = require('./routes/sells');
@@ -6,6 +7,7 @@ const servicesRouter = require('./routes/services');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use('/api/pets', petsRouter);
 app.use('/api/sells', sellsRouter);
